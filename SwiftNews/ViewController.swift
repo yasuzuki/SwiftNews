6907abaaab528fe63785d6f7000c4130a6eeafe4
Yasunori Suzuki <railsyas@gmail.com>
2015-02-07 22:08:20 +0900

    Add initial view.

--- SwiftNews/ViewController.swift
+++ SwiftNews/ViewController.swift
@@ -14,6 +14,40 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    let iphones = [
+        "iPhone",
+        "iPhone 3G",
+        "iPhone 4",
+        "iPhone 4S",
+        "iPhone 5",
+        "iPhone 5s",
+        "iPhone 6",
+        "iPhone 6 Plus"
+    ]
+    
+    let years = [
+        "2007",
+        "2008",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2014"
+    ]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return iphones.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "news")
+        cell.textLabel?.text = iphones[indexPath.row]
+        cell.detailTextLabel?.text = years[indexPath.row]
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
