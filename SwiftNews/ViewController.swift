2b1796c38386785959c5b796b6ec4e60c0f3d698
Yasunori Suzuki <railsyas@gmail.com>
2015-02-07 21:36:38 +0900

    Initial Commit

---
+++ SwiftNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNews
+//
+//  Created by yas on 2015/02/07.
+//  Copyright (c) 2015年 yasuzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
